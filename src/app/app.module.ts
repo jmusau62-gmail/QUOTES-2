e0e1be65520333a38e996f012565ee48549f123f
jmusau62-gmail <jmusau62@gmail.com>
2021-03-28 22:31:37 +0300

    generated quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { TimeCountPipe } from './time-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TimeCountPipe } from './time-count.pipe';
     QuoteComponent,
     QuoteDetailComponent,
     StrikethroughDirective,
-    TimeCountPipe
+    TimeCountPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
