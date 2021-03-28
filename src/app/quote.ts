c6de7082af24c4312215aeff8f377abc5a0058b5
jmusau62-gmail <jmusau62@gmail.com>
2021-03-28 20:52:58 +0300

    added quote -detail component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,5 +2,5 @@
 export class Quote {
     public showDescription: boolean;
     constructor(public id: number,public name: string,public description: string, public completeDate: Date){
-      this.showDescription=false;
+      this.showDescription=false;}
     }
